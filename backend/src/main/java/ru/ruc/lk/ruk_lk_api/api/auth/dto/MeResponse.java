8d656cdad141422ad143b9cc73436c763f7b99e6
gama1726 <gama17one@gmail.com>
2026-06-23 13:08:13 +0300

    feat:добавил dto проверки входа

---
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/dto/MeResponse.java
@@ -0,0 +1,9 @@
+package ru.ruc.lk.ruk_lk_api.api.auth.dto;
+
+import java.util.List;
+
+public record MeResponse(
+    String recordBookNumber,//номер зачётки
+    String fullName,//ФИО
+    List<ProgramSummary> programs//список программ
+){}
