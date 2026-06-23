ec729fec2601d41436ae49c70344c477d9b6a5d5
gama1726 <gama17one@gmail.com>
2026-06-23 12:56:08 +0300

    chore:комментарии

--- backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/dto/LoginRequest.java
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/dto/LoginRequest.java
@@ -1,1 +1,6 @@
-public record LoginRequest(String recordBookNumber,String password){}
+package ru.ruc.lk.ruk_lk_api.api.auth.dto;
+
+public record LoginRequest(
+    String recordBookNumber,//номер зачётки
+    String password//пароль
+    ){}
