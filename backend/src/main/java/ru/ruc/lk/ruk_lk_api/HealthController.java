c2a1539f85f9425c0c279c742b37b0f63241a521
gama1726 <gama17one@gmail.com>
2026-06-23 11:53:02 +0300

    fix:Healt Controller библиотека Map

--- backend/src/main/java/ru/ruc/lk/ruk_lk_api/HealthController.java
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/HealthController.java
@@ -2,7 +2,7 @@ package ru.ruc.lk.ruk_lk_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Map;
 @RestController
 public class HealthController {
 
