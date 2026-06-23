5b4d569d30591fff528b11bf63ee215960fdd575
gama1726 <gama17one@gmail.com>
2026-06-23 11:44:00 +0300

    chore(backend): инициализирован Spring Boot проект
    Добавлены pom.xml, mvnw, RukLkApiApplication и application.properties.

---
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/RukLkApiApplication.java
@@ -0,0 +1,13 @@
+package ru.ruc.lk.ruk_lk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RukLkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RukLkApiApplication.class, args);
+	}
+
+}
