b04569d3896a6e1e51b0191e370188765a68d47c
gama1726 <gama17one@gmail.com>
2026-06-24 11:01:33 +0300

    feat(backend): смена переменной studenID

--- backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/dto/ProgramSummary.java
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/dto/ProgramSummary.java
@@ -2,7 +2,7 @@ package ru.ruc.lk.ruk_lk_api.api.auth.dto;
 public record ProgramSummary(
     String id, //ключ программы во фронте
 
-    String recordBookNumber, //номер зачётки
+    String studentId, //номер зачётки
     String level, //Бакалавриат, Магистратура, Специалитет,Аспирантура
     String direction, //направление плдготовки
     String group //учебная группа
