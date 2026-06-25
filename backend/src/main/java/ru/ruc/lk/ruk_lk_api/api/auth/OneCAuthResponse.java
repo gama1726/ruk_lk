6950d7607e3c36b853b1b803753010a69c389f8a
gama1726 <gama17one@gmail.com>
2026-06-25 11:03:50 +0300

    feat:фио для запроса auth

--- backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/OneCAuthResponse.java
+++ backend/src/main/java/ru/ruc/lk/ruk_lk_api/api/auth/OneCAuthResponse.java
@@ -3,7 +3,8 @@ package ru.ruc.lk.ruk_lk_api.api.auth;
 public record OneCAuthResponse(
     boolean authenticated,
     String studentId,
-    String email
+    String email,
+    String fullname
 ) {
     public OneCAuthResponse {
         if (fullname == null){
