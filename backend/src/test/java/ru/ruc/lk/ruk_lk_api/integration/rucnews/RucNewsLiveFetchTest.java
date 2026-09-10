3d08e39cc0b8ce455b3555ed0ad28ac029c98015
gama1726 <gama17one@gmail.com>
2026-09-10 14:34:56 +0300

    fix:падение сборки докер из-за нового аргумента

--- backend/src/test/java/ru/ruc/lk/ruk_lk_api/integration/rucnews/RucNewsLiveFetchTest.java
+++ backend/src/test/java/ru/ruc/lk/ruk_lk_api/integration/rucnews/RucNewsLiveFetchTest.java
@@ -2,11 +2,14 @@ package ru.ruc.lk.ruk_lk_api.integration.rucnews;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import ru.ruc.lk.ruk_lk_api.metrics.OutboundLoadMetrics;
 
 class RucNewsLiveFetchTest {
 
@@ -20,7 +23,7 @@ class RucNewsLiveFetchTest {
             900,
             12
         );
-        HttpRucNewsClient client = new HttpRucNewsClient(props);
+        HttpRucNewsClient client = new HttpRucNewsClient(props, mock(OutboundLoadMetrics.class));
         List<RucNewsItem> items = client.fetchLatest();
         System.out.println("items=" + items.size() + " ok=" + client.lastFetchOk());
         items.stream().limit(3).forEach(i -> System.out.println(i.date() + " | " + i.title()));
